1ad015033aec2b7d5361c18e58a64b27394976c2
Josh Cheek <josh.cheek@gmail.com>
2012-05-31 02:53:43 -0500

    Fix method missing error where local var became an ivar

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,27 +9,27 @@ module Freezetime
   def two_seconds_ago
     now - 2
   end
-  
+
   def one_second_ago
     now - 1
   end
-  
+
   def now
     Time.utc 2008, 7, 8, 9, 10, 11
   end
-  
+
   def one_second_from_now
     now + 1
   end
-  
+
   def two_seconds_from_now
     now + 2
   end
-  
+
   def freeze_time
     Timecop.freeze now
   end
-  
+
   def unfreeze_time
     Timecop.return
   end
@@ -40,18 +40,18 @@ module ManageServer
   def port
     8080
   end
-  
+
   def url
     "http://0.0.0.0:#{port}/"
   end
-  
+
   def start_server
-    @server_thread = Thread.new { Rack::Server.start app: TimeServer, Port: port, server: 'webrick' } 
+    @server_thread = Thread.new { Rack::Server.start app: TimeServer, Port: port, server: 'webrick' }
     sleep 2
   end
-  
+
   def stop_server
-    server_thread.kill
+    @server_thread.kill
   end
 end
 
